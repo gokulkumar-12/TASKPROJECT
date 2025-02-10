cd1b3f000cd21f01424466def168a4f429ff9d89
gokulkumar <gokulkumar9664@gmail.com>
2025-02-10 19:32:30 +0530

    Initial commit

--- app/src/main/java/com/example/taskapp/AppConstant.kt
+++ app/src/main/java/com/example/taskapp/AppConstant.kt
@@ -5,7 +5,7 @@ class AppConstant {
     companion object{
         public const val BASE_URL = "https://newsapi.org/v2/"
         public const val Q = "tesla";
-        public const val FROM = "2025-01-08"
+        public const val FROM = "2025-01-11"
         public const val SORT_BY = "publishedAt"
         public const val API_KEY = "06e9f422ba5b4f9da1636261793cc233"
         public const val LONG_MILLI_SECONDS = 2000;
